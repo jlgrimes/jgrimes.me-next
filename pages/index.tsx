926bf0323f90dc5b5cec58a93f47737c2bdea49d
Jared Grimes <jaredlgrimes@hotmail.com>
2022-11-29 23:36:08 -0500

    fixed spacing mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <Stack justifyContent='center' alignItems='center'>
       <Stack spacing='8' paddingTop='200' paddingLeft='16'>
-        <Heading margin='0' color='#2D3748' fontSize='5rem'>{`Hi, I'm Jared.`}</Heading>
+        <Heading
+          margin='0'
+          lineHeight='5rem'
+          color='#2D3748'
+          fontSize='5rem'
+        >{`Hi, I'm Jared.`}</Heading>
         <Heading
           color='#4A5568'
           size='2xl'
